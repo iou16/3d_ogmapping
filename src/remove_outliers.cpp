6fe947c1816fe941702c24aa11db8c07281a450a
iou16 <kappasetuhirobunbun@gmail.com>
2015-12-06 00:15:14 +0900

    ちょっとスマートに

--- src/remove_outliers.cpp
+++ src/remove_outliers.cpp
@@ -17,41 +17,54 @@
 #include <pcl/filters/voxel_grid.h>
 #include <pcl/filters/statistical_outlier_removal.h>
 
+
+#include <iostream>
+#include <string>
+
+#include <boost/program_options.hpp>
+
+
 class RemoveOutliersMap
 {
   public:
     RemoveOutliersMap() :
       rate_(10)
     {
-      ros::NodeHandle private_nh("~");
       ros::NodeHandle nh;
       map_cloud_inliers_pub_ = nh.advertise<sensor_msgs::PointCloud2>("map_cloud_inliers", 0, true);
       map_cloud_outliers_pub_ = nh.advertise<sensor_msgs::PointCloud2>("map_cloud_outliers", 0, true);
     }
 
     void run() {
-      while (ros::ok()) {
       pcl::PointCloud<pcl::PointXYZ>::Ptr map_cloud (new pcl::PointCloud<pcl::PointXYZ>);
       pcl::PCDReader reader;
-      reader.read<pcl::PointXYZ> ("3d_map.pcd", *map_cloud);
+      reader.read<pcl::PointXYZ> ("3d_map2.pcd", *map_cloud);
+
       pcl::StatisticalOutlierRemoval<pcl::PointXYZ> sor;
       sor.setInputCloud (map_cloud);
       sor.setMeanK (50);
       sor.setStddevMulThresh (1.0);
+
       pcl::PointCloud<pcl::PointXYZ>::Ptr map_cloud_inliers(new pcl::PointCloud<pcl::PointXYZ>);
       sor.filter (*map_cloud_inliers);
+
+      sensor_msgs::PointCloud2 map_cloud_inliers_msg;
       pcl::toROSMsg(*map_cloud_inliers,map_cloud_inliers_msg);
+      map_cloud_inliers_msg.header.frame_id = "odom";
 
       sor.setNegative(true);
       pcl::PointCloud<pcl::PointXYZ>::Ptr map_cloud_outliers(new pcl::PointCloud<pcl::PointXYZ>);
       sor.filter (*map_cloud_outliers);
+
+      sensor_msgs::PointCloud2 map_cloud_outliers_msg;
       pcl::toROSMsg(*map_cloud_outliers,map_cloud_outliers_msg);
+      map_cloud_outliers_msg.header.frame_id = "odom";
+
+      while (ros::ok()) {
         map_cloud_inliers_msg.header.stamp = ros::Time::now();
-        map_cloud_inliers_msg.header.frame_id = "odom";
         map_cloud_inliers_pub_.publish(map_cloud_inliers_msg);
 
         map_cloud_outliers_msg.header.stamp = ros::Time::now();
-        map_cloud_outliers_msg.header.frame_id = "odom";
         map_cloud_outliers_pub_.publish(map_cloud_outliers_msg);
 
         ros::spinOnce();
@@ -60,9 +73,6 @@ class RemoveOutliersMap
     }
 
   private:
-    sensor_msgs::PointCloud2 map_cloud_inliers_msg;
-    sensor_msgs::PointCloud2 map_cloud_outliers_msg;
-
     ros::Publisher map_cloud_inliers_pub_;
     ros::Publisher map_cloud_outliers_pub_;
 
